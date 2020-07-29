408fde19ea28744422b74abd15b5c33023a07862
Olena Sukhanova <asukhanova87@gmail.com>
2020-07-29 14:33:16 +0300

    added contact.html and about.html

--- app.js
+++ app.js
@@ -32,8 +32,72 @@ mobileMenu.addEventListener("click", function(){
 AOS.init({
     // easing: 'ease-in-out-sine',
     easing:'ease',
-    duration: 300,
+    duration: 500,
     delay: 0,
-    once: true,
+    once: false,
   });
   
+
+
+  // counter
+
+  $('.customers').animationCounter({
+      start: 0,
+      end: 500,
+      step: 10,
+      delay:80
+    });
+    
+    $('.photos').animationCounter({
+      start: 0,
+      end: 1000,
+      step: 20,
+      delay:80
+    });
+
+    $('.foods').animationCounter({
+      start: 0,
+      end: 300,
+      step: 6,
+      delay:80
+    });
+
+    $('.experience').animationCounter({
+      start: 0,
+      end: 12,
+      step: 1,
+      delay:330,
+    });
+
+  
+    // map
+    jQuery('#vmap').vectorMap(
+      {
+          map: 'world_en',
+          // backgroundColor: '#a5bfdd',
+          backgroundColor:'none',
+          borderColor: '#818181',
+          borderOpacity: 0.25,
+          borderWidth: 1,
+          color: '#f4f3f0',
+          enableZoom: true,
+          // hoverColor: '#c9dfaf',
+          hoverColor: '#fed741',
+          hoverOpacity: null,
+          normalizeFunction: 'linear',
+          scaleColors: ['#b6d6ff', '#005ace'],
+          // selectedColor: '#c9dfaf',
+          selectedColor: '#fed741',
+          selectedRegions: null,
+          showTooltip: true,
+          onRegionClick: function(element, code, region)
+    {
+        var message = 'You clicked "'
+            + region
+            + '" and we are located in >> LOOK BELOW'
+            // + code.toUpperCase();
+
+        alert(message);
+    }
+         
+      });
